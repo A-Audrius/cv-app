3ae8de4134611b98db992718103c2424604f9c8d
AAdomavicius <audriusad@gmail.com>
2021-07-17 16:48:06 +0300

    0717

---
+++ js/app.js
@@ -0,0 +1,55 @@
+
+document.querySelector('#my-name').innerHTML = `${data.first_name} <span class="color-grey">${data.last_name}</span>`;
+document.querySelector('#job-title').textContent = data.job_title;
+document.querySelector('.profile-data').textContent = data.profile;
+
+// 
+// 
+
+let expirienceData = '';
+data.experience.forEach(item => {
+    expirienceData += `<section class="mb-5">`;
+    expirienceData += `<h6><a href="${item.site_address}">${item.site_address}</a></h6>`;
+    expirienceData += `<p>${item.description}</p>`;
+    expirienceData += `</section>`;
+});
+document.querySelector('.experience-list').innerHTML = expirienceData;
+
+
+
+// contact
+
+document.querySelector('.contacts-block').innerHTML += `<li><a href="tel:${data.phone}">${data.phone}</a></li>`
+document.querySelector('.contacts-block').innerHTML += `<li><a href="mailto:${data.email}">${data.email}</a></li>`
+document.querySelector('.contacts-block').innerHTML += `<li><a href="${data.linkedin}">linkedin....</a></li>`
+document.querySelector('#my-photo').src = data.photo;
+
+// skills
+
+let skillsData = '';
+data.skills.forEach(item => {
+    skillsData += `<li>${item}</li>`;
+});
+document.querySelector('.skills-list').innerHTML = skillsData;
+
+
+
+/**
+ * education
+ */
+
+ let educationData = '';
+ data.education.forEach(item => {
+     educationData += `<li>${item[0]}, ${item[1]}</li><br>`;
+ });
+ document.querySelector('.education-list').innerHTML = educationData;
+
+
+ 
+// * Achievements
+
+let achievementsData = '';
+data.achievements.forEach(item => {
+   achievementsData += `<li>${item},</li>`;
+});
+document.querySelector('.achievements-list').innerHTML = achievementsData;
---
+++ js/data.js
@@ -0,0 +1,37 @@
+
+
+const data = {
+    "first_name": "Audrius",
+    "last_name": "Adomavičius",
+    "job_title": "frontend programuotojas",
+    "photo": "images/foto.jpg",
+    "phone": "tel: +37065087335",
+    "email": "audriusad@gmail.com",
+    "linkedin": "https://www.linkedin.com/in/audrius-a-859ab451",
+
+    "skills": [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "Wordpress",
+        "Angular",
+    ],
+    "education": [
+        ["2020-2021", "SOFTWARE DEVELOPMENT ACADEMY" ],
+        ["2001-2005", "VILNIAUS DAILĖS AKADEMIJA"],
+    ],
+    "profile": "Esu.......",
+
+    "experience": [
+        {
+            "site_address": "http://rigra.lt",
+            "description": "Tai mano pirmasis projektas: internetinis puslapis rigra.lt. Visas kodas parašytas nuo nulio, perkeltas į Wordpress sistemą, po to ir į nuotolinį serverį. Žinoma ne viskas šiame puslapyje tobula, bet informacija suvaikšto pakankamai sparčiai, viskas kas reikia tvarkingai atsidaro ir užsidaro."
+        },
+        {
+            "site_address": "https://aadomavicius.github.io/Book-shop-project/",
+            "description": 'Visada norėjau sukurti kažką tokio kaip elekroninę parduotuvę kur duomenys atkeliauja iš nuotolinio serverio(šiuo atveju duomenys ) ir "stebuklingu būdu" atsivaizduoja naršyklėje. Įdomu buvo, kad galėjau panaudoti OOP(objektinį programavimą. Šis projektėlis dar tesiamas..'
+        },
+  
+    ]
+    
+};
